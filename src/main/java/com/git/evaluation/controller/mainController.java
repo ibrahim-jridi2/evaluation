c4b1d38e630b51fc7d94d519cfb847c4d668944f
i.jridi <ibrahim.jridi@esprit.tn>
2023-02-06 08:52:58 +0100

    main commit

---
+++ src/main/java/com/git/evaluation/controller/mainController.java
@@ -0,0 +1,10 @@
+package com.git.evaluation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/esprit")
+public class mainController {
+    // for all , this class will be touched later .
+}
---
+++ src/main/java/com/git/evaluation/services/mainService.java
@@ -0,0 +1,8 @@
+package com.git.evaluation.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class mainService {
+    // for all , this class will be touched later .
+}
