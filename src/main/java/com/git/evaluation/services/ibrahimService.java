83ebcc5a52af97923cbe0beb58111b4c07f37675
i.jridi <ibrahim.jridi@esprit.tn>
2023-02-06 10:37:49 +0100

    ibrahim first commit

---
+++ src/main/java/com/git/evaluation/services/ibrahimService.java
@@ -0,0 +1,10 @@
+package com.git.evaluation.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ibrahimService {
+    public void ibrahimMsg(){
+        System.out.println("this is ibrahim , Hello everyone");
+    }
+}
