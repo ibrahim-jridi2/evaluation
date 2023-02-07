53ed9e4dcde91ee8f2105861679fe10e9ac9b9f1
i.jridi <ibrahim.jridi@esprit.tn>
2023-02-07 21:40:06 +0100

    hachem ibrahim commit

--- src/main/java/com/git/evaluation/controller/mainController.java
+++ src/main/java/com/git/evaluation/controller/mainController.java
@@ -1,5 +1,6 @@
 package com.git.evaluation.controller;
 
+import com.git.evaluation.services.HachemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/esprit")
 public class mainController {
-
+    @Autowired
+    HachemService hech;
 
     // for all , this class will be touched later .
     //for all : in this main controller we gonna try to use others service in our own controller.
-    // for exp: i'm gonna write a method called public void ibrahimMsg() and into it i will call hachem service 
+    // for exp: i'm gonna write a method called public void ibrahimMsg() and into it i will call hachem service
+    @GetMapping
+    public String IbrahimMsgFromHachemService(){
+        return hech.
+
+    }
 
 }
