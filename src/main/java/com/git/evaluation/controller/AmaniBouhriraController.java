b5fa5b9fb4a2722f2f1d63b3d175890f11e51f09
Bouhrira Amani <a.bouhrira@numeryx.fr>
2023-02-07 07:34:30 +0100

    Amani Bouhrira First commit!

---
+++ src/main/java/com/git/evaluation/controller/AmaniBouhriraController.java
@@ -0,0 +1,20 @@
+package com.git.evaluation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.git.evaluation.services.AmaniBouhriraService;
+
+@RestController
+@RequestMapping(value = "/alternance")
+public class AmaniBouhriraController {
+    @Autowired
+    AmaniBouhriraService AmaniBouhriraService;
+
+    // for all , this class will be touched later .
+    @GetMapping(value = "/AmaniBouhrira")
+    public String AmaniBouhriraMsg() {
+        return AmaniBouhriraMsg.AmaniBouhriraMsg();
+    }
+}
---
+++ src/main/java/com/git/evaluation/services/AmaniBouhriraService.java
@@ -0,0 +1,10 @@
+package com.git.evaluation.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AmaniBouhriraService {
+    public String AmaniBouhriraMsg(){
+        return "this is Amani Bouhrira , Hello everyone";
+    }
+}
